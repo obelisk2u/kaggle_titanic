8d5567a7cc1fb36f78ffdecfcc248ef44d4e157a
Jordan <jdstout@gmail.com>
2024-09-09 16:18:50 -0400

    PLEASE LET MY GUY SEE THE FILE

--- hopefully_the_idea.py
+++ hopefully_the_idea.py
@@ -1,4 +1,5 @@
 import autogen
+import pandas
 
 config_list = [
     {
@@ -8,7 +9,6 @@ config_list = [
 ]
 
 llm_config={
-    #"request_timeout": 600, 
     "seed": 42, 
     "config_list": config_list,
     "temperature": 0
@@ -26,13 +26,13 @@ user_proxy = autogen.UserProxyAgent(
     human_input_mode = "TERMINATE",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content","").rstrip().endswith("TERMINATE"),
-    
     llm_config=llm_config,
     system_message="""reply terminate if solved, else reply continue"""
 )
 
-task = """
-Solve the Kaggle Titanic competition using train.csv in my working directory as the training data, output the code used into a file called sub.py"""
+task = r"""
+Solve the Kaggle Titanic competition using C:\Users\jdsto\OneDrive\Documents\kaggle_titanic\train.csv as the training data, output the code used into a file called sub.py"""
+
 user_proxy.initiate_chat(
     assistant,
     message=task
